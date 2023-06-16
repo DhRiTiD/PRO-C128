060c044bac2d66a439e3b49974053e3d5162d79d
Dhriti Dhall <74966124+DhRiTiD@users.noreply.github.com>
2023-06-16 11:32:34 +0530

    Update drawf_stars_data_scraper.py to a better ver

--- drawf_stars_data_scraper.py
+++ drawf_stars_data_scraper.py
@@ -1,41 +1,19 @@
-from bs4 import BeautifulSoup as bs
-import requests
 import pandas as pd
+import requests
+import selenium
+from bs4 import BeautifulSoup as bs
 
-url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+url = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs#Field_brown_dwarfs"
 
 page = requests.get(url)
+soup = bs(page.text, 'html.parser')
 
-soup = bs(page.text,'html.parser')
+star_table = soup.find_all('table')
 
-star_table = soup.find_all('table', {"class":"wikitable sortable"})
+print(star_table)
 
-total_table = len(star_table)
+df=pd.read_html(str(star_table[7]))
+df=pd.DataFrame(df[0])
+print(df.head())
 
-
-temp_list= []
-table_rows = star_table[1].find_all('tr')
-
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text.rstrip() for i in td]
-    temp_list.append(row)
-
-Star_names = []
-Distance =[]
-Mass = []
-Radius =[]
-
-print(temp_list)
-
-for i in range(1,len(temp_list)):
-    
-    Star_names.append(temp_list[i][0])
-    Distance.append(temp_list[i][5])
-    Mass.append(temp_list[i][7])
-    Radius.append(temp_list[i][8])
-
-headers = ['Star_name','Distance','Mass','Radius']  
-df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
-
-df2.to_csv('dwarf_stars.csv', index=True, index_label="id")
+df.to_csv('brown_planet_data.csv')
